00bb93122e3540a5f254f719420426e7b14c4901
Unknown <soenke.krusche@gmail.com>
2019-01-18 17:01:52 +0100

    Remove unused import

--- src/main/java/de/soenkekrusche/example/Main.java
+++ src/main/java/de/soenkekrusche/example/Main.java
@@ -1,7 +1,6 @@
 package de.soenkekrusche.example;
 
 import javafx.application.Application;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
